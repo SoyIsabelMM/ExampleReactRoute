1b84b1a4025a54b10d452fa9220ddca5b65f1774
Isa <isa@MacBook-Air-de-Daniel.local>
2023-08-27 15:59:27 -0400

    feat: first Commit in the repositore

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Dashboard.css";
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <h1>El analista de Emojis: Todo sobre los Emojis</h1>
+      <p>¡El destino # de opiniones sobre Emojis en la Web desde 2020!</p>
+    </div>
+  );
+}
+
+export default Dashboard;
